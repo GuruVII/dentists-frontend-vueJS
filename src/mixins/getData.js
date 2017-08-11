5da973585cec5b73b8f0e2979305547d826907d9
GuruVII <guruvii@gmail.com>
2017-08-11 21:44:23 +0200

    changed REST API location to an online one

--- src/mixins/getData.js
+++ src/mixins/getData.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
 	methods: {
 		getData: function(oe, offset, maxAvg,type,sortColumn, orderBy){
-			axios.get(`http://localhost:3000/Dentists/${oe}&offset=${offset}&maxAvg=${maxAvg}&type=${type}&${sortColumn}=${orderBy}`, {
+			axios.get(`https://dentist-backend.herokuapp.com/Dentists/${oe}&offset=${offset}&maxAvg=${maxAvg}&type=${type}&${sortColumn}=${orderBy}`, {
 				})
 				.then(response => {
 					console.log(`http://localhost:3000/Dentists/${oe}&offset=${offset}&maxAvg=${maxAvg}&type=${type}&${sortColumn}=${orderBy}`)
